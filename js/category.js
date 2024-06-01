39efb32dabc32ce78859d9bd34a07177bdd1eac8
vickycastano <vcastanoseippel@udesa.edu.ar>
2024-06-01 15:13:34 -0300

    js de category

--- js/category.js
+++ js/category.js
@@ -3,15 +3,19 @@ fetch('https://fakestoreapi.com/products')
 return response.json();
 })
 .then(function(data){
-console.log(data)
 
+let arrayproductos1 = data;
+let section = document.querySelector('.conteiner')
 
-///
-let vermas = document.querySelector('.botonvermas')
-vermas.addEventListener('click', function(){
-let vermas = document.querySelector('.botonvermas').innerHTML = URL
-})
+for (let i = 14; i < arrayproductos1.length; i++) {
+section.innerHTML += ` <section>
+<h1>${arrayproductos1[i].title} </h1>
+<p>${arrayproductos1[i].description} </p>
+<h3>${arrayproductos1[i].price} </h3>
+<img src=${arrayproductos1[i].image} />
+</section>`
 
+}
 
 })
 .catch(function(error){
