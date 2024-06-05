870f05c425b92e32c4b3aaf11195c7b4d33d21e3
vickycastano <vcastanoseippel@udesa.edu.ar>
2024-06-05 16:11:33 -0300

    terminar el codigo js de login de la 6a

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 fetch('https://fakestoreapi.com/products')
 
-
 .then(function(response){
     return response.json()
 })
 .then(function(data){
   console.log(data)
+
+  let 
 
 
 
--- js/login.js
+++ js/login.js
@@ -1,28 +1,31 @@
 
 let inputemail = document.querySelector('#email')
 let inputcontrasena = document.querySelector('#contrasena') 
-
 let form = document.querySelector('.formulario')
+let contrasenacorrecta = false 
+let emailcorrecto = false 
 
 form.addEventListener('submit', function(e){
-//e.preventDefault();
 if (inputemail.value == '') { 
 alert("Por favor complete el campo email")
+}else { 
+  emailcorrecto = true 
+
 }
 if (inputcontrasena.value == '') {
     alert("Por favor complete el campo contraseña")
-  }else if (inputcontrasena.value.length <6 )
+   
+  }else if (inputcontrasena.value.length <6 ){
     alert("La contraseña debe tener al menos 6 caracteres" )
-  })
 
+  }else {
+    contrasenacorrecta = true 
 
-
-
-
-
-
-
-
+  }if (!contrasenacorrecta || !emailcorrecto ) {
+    e.preventDefault()
+  }
+})
+  
 // guardar en localStronge // 
 
 let QS = location.search
