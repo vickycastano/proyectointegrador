5e559a9b997134f5a0067c11a99e2801c8a430fe
vickycastano <vcastanoseippel@udesa.edu.ar>
2024-06-07 13:07:08 -0300

    cambios en el js de login

--- js/login.js
+++ js/login.js
@@ -5,6 +5,8 @@ let form = document.querySelector('.formulario')
 let contrasenacorrecta = false 
 let emailcorrecto = false 
 
+//VER
+
 form.addEventListener('submit', function(e){
 if (inputemail.value == '') { 
 alert("Por favor complete el campo email")
@@ -24,32 +26,8 @@ alert("Por favor complete el campo email")
   }
 })
   
+
+
 // guardar en localStronge // 
 
-let QS = location.search
-let QS1 = new URLSearchParams(QS)
-
-let searchQS = QS1.get('email')
-
-let qs = location.search
-let qs1 = new URLSearchParams(qs)
-let email = qs1.get('email')
-
-console.log(email)
-
-let QSS = location.search
-let QSS1 = new URLSearchParams(QSS)
-
-let searchQS1 = QSS1.get('contrasena')
-
-let qss = location.search 
-let qss1 = new URLSearchParams(qss)
-let contrasena = qss1.get('contrasena')
-
-console.log(contrasena)
-  
-if (email){
-  document.querySelector('.saludo').innerText= "Bienvenido:" + email + ""
-}
-console;log(email)
 
