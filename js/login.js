e66f193c01387f7db183b21e98fd7af7aaa7d1c5
vickycastano <vcastanoseippel@udesa.edu.ar>
2024-06-07 17:39:11 -0300

    mejoras del login js despues de la explicacion en clase

--- js/login.js
+++ js/login.js
@@ -2,27 +2,27 @@
 let inputemail = document.querySelector('#email')
 let inputcontrasena = document.querySelector('#contrasena') 
 let form = document.querySelector('.formulario')
-let contrasenacorrecta = false 
-let emailcorrecto = false 
 
 //VER
 
 form.addEventListener('submit', function(e){
+e.preventDefault()
+
+
 if (inputemail.value == '') { 
 alert("Por favor complete el campo email")
-}else { 
-  emailcorrecto = true 
-}if (inputcontrasena.value == '') {
+}
+
+if (inputcontrasena.value == '') {
     alert("Por favor complete el campo contraseña")
    
   }else if (inputcontrasena.value.length <6 ){
     alert("La contraseña debe tener al menos 6 caracteres" )
 
-  }else {
-    contrasenacorrecta = true 
 
-  }if (!contrasenacorrecta || !emailcorrecto ) {
-    e.preventDefault()
+  }else {
+    
+this.submit()
   }
 })
   
