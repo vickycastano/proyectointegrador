b107b5b062b56b8650ea74e887cbcbc5104ee67e
vickycastano <vcastanoseippel@udesa.edu.ar>
2024-06-09 14:21:29 -0300

    cambios en el  js de categoria

--- js/category.js
+++ js/category.js
@@ -20,45 +20,7 @@ navegador.innerHTML =  `
   <li> <a href="./category.html?category=${data[3]}">ROPA DE MUJER</a></li>
 </ul>
 ` 
-
-//NO FUNCIONA 
-if (data[3]) {
-  let categorias = data 
-let cambiocategorias = document.querySelector('.categorias')
-arraycategorias = []
-
-
-fetch('https://fakestoreapi.com/products/category/jewelery')
-.then(function(response){
-  return response.json()
-})
-.then(function(data){
-  console.log(data)
-
-for (let i = 0; i < categorias.length; i++) {
-  arraycategorias +=  ` 
-  <div>
-   <img src="${categorias[i].image}" >
-  <h1>${categorias[i].title}</h1>
-  <p>${categorias[i].description}</p>
-  <h3>${categorias[i].price}</h3>
-  <a  href="./producto.html?id=${categorias[i].id}">VER MAS</a>
-  </div>  ` 
-
-}
 })
 .catch(function(error){
   console.log(error)
 })
-
-cambiocategorias.innerHTML = arraycategorias
-
-}
-
-
-})
-.catch(function(error){
-  console.log(error)
-})
-
-
