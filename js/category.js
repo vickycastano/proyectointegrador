17b052221d1ec348a64ae5d0100be2a5166e9a44
vickycastano <vcastanoseippel@udesa.edu.ar>
2024-06-12 16:18:42 -0300

    segui intentando con caegory js

--- js/category.js
+++ js/category.js
@@ -1,22 +1,48 @@
-let queryString = location.search;
-let urlParams = new URLSearchParams(queryString);
-let categoria = urlParams.get('category');
+//QueryString
+let QueryString = location.search;
+let queryStringOBJ = new URLSearchParams(QueryString);
+let category = queryStringOBJ.get("categories");
+console.log(category);
 
-
-urlcategory = `https://fakestoreapi.com/products/categories/${categoria}`
-
-fetch(urlcategory)
+ 
+urlcategorias = `https://fakestoreapi.com/products/category/${category}`
+fetch(urlcategorias)
 .then(function(response){
-return response.json()
+    return response .json();
 })
 .then(function(data){
-console.log(data)
+    console.log(data);
+//    let informacioncategorias = data
 
-// terminar de rellenar 
+//for (let i = 0; i < informacioncategorias.length; i++) {
+//    arraycateogria +=  
+//    `<div>
+// <img src="${informacioncategorias[i].image}" >
+//  <h1>${informacioncategorias[i].title}</h1>
+//  <p>${informacioncategorias[i].description}</p>
+//  <h3>${informacioncategorias[i].price}</h3>
+//  <a  href="./producto.html?id=${informacioncategorias[i].id}">VER MAS</a>
+//  </div>`
+//}
+
+
+//section.innerHTML = arraycateogria
+    
+
+//let navegador = document.querySelector('.navegador1')
+
+//navegador.innerHTML =  `  
+//<ul class="navegador">
+//  <li><a href="./index.html">HOME</a> </li>
+//  <li> <a href="./category.html?category=${informacioncategorias.category="electronics"}">ELECTRONICOS</a></li>
+//  <li><a href="./category.html?category=${informacioncategorias.category="jewelery"}">ACCESSORIOS</li> 
+//  <li> <a href="./category.html?category=${informacioncategorias.category="women's clothing"}">ROPA DE MUJER</a></li>
+//</ul>
+//` 
+
 
 
 })
 .catch(function(error){
-console.log(error)
+console.log(error);
 })
-
