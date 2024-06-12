53bc1b8388695422381dcee4f6107be49a4fbe52
NicoledeLeone23 <ndeleone@udesa.edu.ar>
2024-06-11 22:47:49 -0300

    Punto 3 empezadp (producto.js)

--- js/producto.js
+++ js/producto.js
@@ -1,12 +1,32 @@
-fetch('https://fakestoreapi.com/products/1')
+// traer id de producto seleccionado
+let qString = location.search; // esto lo trae como cadna de texto, es '?clave=valor" por ejemplo ?id=3
+//cambio eso a un objeto literal para accesar el valor
+let qStringObj = new URLSearchParams(qString);
+let numId = qStringObj.get("id"); 
+
+let url = 'https://fakestoreapi.com/products/${numId}'
+
+fetch(url)
 .then(function(response){
     return response.json();
-    })
-    .then(function(data){
-        console.log(data)
+})
+.then(function(data){
+    console.log(data);
+    let nombre = document.querySelector(".nombreProducto");
+    let descrip = document.querySelector(".descripcion");
+    let imagen = document.querySelector(".im");
+    let categoria = document.querySelector(".descriphome");
+    let precio = document.querySelector(".precio");
+    let carritoBut = document.querySelector(".boton");
 
+    nombre.innerText+=data.title;
+    descrip.innerText+=data.description;
+    imagen.src=data.image;
+    categoria.innerText+=data.category;
+    precio.innerText+=data.price;
+    carritoBut
     
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+})
+.catch(function(error){
+    console.log(error);
+})
