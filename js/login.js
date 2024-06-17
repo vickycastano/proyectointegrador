d11bbff5c2c89580fa11fdedf4b5be5304cb22ee
vickycastano <vcastanoseippel@udesa.edu.ar>
2024-06-17 19:20:18 -0300

    hice que el navegador funcione en cualqier pagina que quieras navegar

--- js/login.js
+++ js/login.js
@@ -1,3 +1,33 @@
+// NAVEGADOR (sin esto en index.js, hace que cuando tocas una categoria entre a una pagina en blanco 
+//(solo el hedear y footer sin la informacion de los apis) y
+// tengas que volver a tocar el boton de la categoria para que se muestre, por eso neceista esar en el js de index,
+// asi te lleva directo a las categorias sin ningun problema)
+
+let queryString = location.search;
+let queryStringOBJ = new URLSearchParams(queryString);
+let category = queryStringOBJ.get("categories");
+
+fetch(`https://fakestoreapi.com/products/category/${category}`)
+.then(function(response){
+    return response .json();
+})
+.then(function(data){
+    console.log(data);
+    let informacioncategoria = data;
+    let navegador = document.querySelector('.navegador')
+
+navegador.innerHTML =  ` 
+  <li><a href="./index.html">HOME</a></li>
+  <li><a href="./category.html?categories=${informacioncategoria.category="electronics"}">ELECTRONICOS</a></li>
+  <li><a href="./category.html?categories=${informacioncategoria.category="jewelery"}">ACCESSORIOS</a></li>
+  <li><a href="./category.html?categories=${informacioncategoria.category="women's clothing"}">ROPA DE MUJER</a></li>`
+
+
+})
+.catch(function(e){
+    console.log(e);
+})
+
 let email = document.querySelector('#email')
 let inputcontrasena = document.querySelector('#contrasena') 
 let form = document.querySelector('.formulario')
